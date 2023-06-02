0e1e1fa2cb3d862475fbcfd8e61fd61943d91b21
Edir P Santos <fennder@gmail.com>
2023-06-02 17:36:43 -0300

    Repassando em aula tds

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
-  has_many :positions # atualiado
+  has_many :positions # atualizado
   has_one_attached :logo
   validates :name, :url, presence: true
 end
