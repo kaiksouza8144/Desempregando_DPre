8b5f6ccc56b757c139d65e11cabbeb55aa6635c8
Edir P Santos <fennder@gmail.com>
2023-07-04 17:59:27 -0300

    🚧 Adiciona .env de exemplo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    binding.pry
     @q = Position.ransack(params[:q])
     @positions = @q.result.page(params[:page]).per(params[:per])
     @contracts = [['CLT', 0], ['PJ', 1], ['A combinar', 2]]
