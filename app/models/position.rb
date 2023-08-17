251e69fa10b7f2343a0e5543b0c56e5e74f2cce3
super choque <thalesrjr5@gmal.com>
2023-08-17 17:49:01 -0300

    teste is_fulltime

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,14 +2,14 @@ class Position < ApplicationRecord
   belongs_to :company
   has_many :applicants
 
-  enum career: [:developer, :business_inteligence, :information_technology, 
+  enum career: [:developer, :business_inteligence, :information_technology,
                 :design, :product, :technology, :other]
   enum contract: [:clt, :pj, :match]
 
   validates :name, :career, :contract, :city, :state, :summary, presence: true
 
   before_save :set_slug
-  
+
   def is_remote
     if self.remote == true
       return true
@@ -18,8 +18,16 @@ class Position < ApplicationRecord
     end
   end
 
+  def is_full_time
+    if self.full_time == true
+      return true
+    else
+      return false
+    end
+  end
 
-    
+
+
 
   private
 
@@ -30,7 +38,7 @@ class Position < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["career", "city", "company_id", "contract", "created_at", "description", "id", "name", "publish", "remote", "slug", "state", "summary", "updated_at"]
   end
-  
- 
-  
+
+
+
 end
